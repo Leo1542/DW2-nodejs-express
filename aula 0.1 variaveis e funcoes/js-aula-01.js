d853626f40ae72295c2f69e0437d508b0a8bc249
Leo1542 <leo.matheus2002@hotmail.com>
2025-02-13 19:05:54 -0300

    Variáveis e funções no JS

---
+++ aula 0.1 variaveis e funcoes/js-aula-01.js
@@ -0,0 +1,9 @@
+const nome = "diego";
+
+var cor = "azul";
+var cor = "Amarelo";
+let cidade = "Registro"
+cidade = "Jacupiranga"
+
+const message = "Hello, world! iniciando em JS!"
+console.log(message)
